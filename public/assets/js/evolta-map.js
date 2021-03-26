ee9ee6f992c80e3e9ed5625cb32d4d83b9a1c11a
venkat9507 <70470427+venkat9507@users.noreply.github.com>
2021-03-26 18:55:31 +0530

    About Us

---
+++ public/assets/js/evolta-map.js
@@ -0,0 +1,30 @@
+/*==============================================================*/
+// DigisailorMap  JS
+/*==============================================================*/
+(function($) {
+    "use strict"; // Start of use strict
+    var marker;
+
+    window.initMap = function() {
+        var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 13,
+        center: {lat: 8.788779, lng: 78.1359641}
+        });
+
+        marker = new google.maps.Marker({
+        map: map,
+        draggable: true,
+        animation: google.maps.Animation.DROP,
+        position: {lat: 8.7930081, lng: 78.1206135}
+        });
+        marker.addListener('click', toggleBounce);
+    }
+
+    function toggleBounce() {
+        if (marker.getAnimation() !== null) {
+        marker.setAnimation(null);
+        } else {
+        marker.setAnimation(google.maps.Animation.BOUNCE);
+        }
+    }
+}(jQuery)); // End of use strict
